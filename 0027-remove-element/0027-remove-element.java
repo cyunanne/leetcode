120373955571e0e8791a2c18c1a9b568c3eff1f8
최유나 <79459256+cyunanne@users.noreply.github.com>
2023-07-22 21:55:51 +0900

    Time: 0 ms (100.00%), Space: 40.9 MB (81.56%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,14 +1,28 @@
+// #1
+// class Solution {
+//     public int removeElement(int[] nums, int val) {
+//         int count = nums.length;
+//         for(int i=0; i<count;) {
+//             if(nums[i] != val) {
+//                 i++;
+//                 continue;
+//             }
+//             for(int j=i+1; j<count; j++)
+//                 nums[j-1] = nums[j];
+//             count--;
+//         }
+//         return count;
+//     }
+// }
+
+// #2
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int count = nums.length;
-        for(int i=0; i<count;) {
+        int count = 0;
+        for(int i=0; i<nums.length; i++) {
             if(nums[i] != val) {
-                i++;
-                continue;
+                nums[count++] = nums[i];
             }
-            for(int j=i+1; j<count; j++)
-                nums[j-1] = nums[j];
-            count--;
         }
         return count;
     }
