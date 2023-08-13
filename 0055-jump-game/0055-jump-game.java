f07f2a0e1ba368b50c2afa71fd20cd86d09ec71d
최유나 <79459256+cyunanne@users.noreply.github.com>
2023-08-13 11:57:37 +0900

    Time: 89 ms (20.68%), Space: 44.5 MB (50.54%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,4 +1,6 @@
 class Solution {
+    
+    // 798ms, 44.4mb
     public boolean canJump(int[] nums) {
         boolean[] reachable = new boolean[nums.length];
         reachable[nums.length-1] = true;
@@ -6,8 +8,7 @@ class Solution {
         for(int i=nums.length-2; i>=0; i--) {
             for(int j=nums[i]; j>0; j--) {
                 int nextIdx = i + j;
-                if(nextIdx >= nums.length) continue;
-                if(reachable[nextIdx]) {
+                if(nextIdx >= nums.length || reachable[nextIdx]) {
                     reachable[i] = true;
                     break;
                 }
